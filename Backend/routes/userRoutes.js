a1bb7a0cdb0750d4ddc35055c5bb2fd8e16d39af
Vikram-Rai0 <ccodework24@gmail.com>
2025-07-19 22:47:37 +0545

    just re-structured folder and files

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/signup", userSignup);
-router.post("/login", userLogin); 
+router.post("/login", userLogin);
 router.get("/:user_id", selectUser);
 router.put("/:user_id", updateUser);
 router.delete("/:user_id", deleteUser);
